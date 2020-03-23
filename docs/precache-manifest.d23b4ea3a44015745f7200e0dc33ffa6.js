f8cd8fb0092899447c85cc66ba644a378f1d180c
Hoang Anh Tuan <hoangtuan1501@gmail.com>
2020-03-23 11:22:29 +0700

    Add distribution folder
    Name this folder 'docs' for Github page

---
+++ docs/precache-manifest.d23b4ea3a44015745f7200e0dc33ffa6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70281784664dccb30fe6",
+    "url": "/css/chunk-vendors.5f8adfd2.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9e7a52871de91a027bd616ba86287ab6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c129ac827edbc27ad0d1",
+    "url": "/js/about.d2182e80.js"
+  },
+  {
+    "revision": "b9b9046d99ab786e5594",
+    "url": "/js/app.b7cd1718.js"
+  },
+  {
+    "revision": "70281784664dccb30fe6",
+    "url": "/js/chunk-vendors.9f51fec6.js"
+  },
+  {
+    "revision": "8c64f261d8ec113545f21dd5ebb7a30b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
